28924858065b77d5e4480e1673695cd0df1b262d
yyy2015 <2456700983@qq.com>
2017-06-25 21:34:08 +0800

    Modify some config

--- src/main/java/edu/nju/student/model/个人信息类型.java
+++ src/main/java/edu/nju/student/model/个人信息类型.java
@@ -31,7 +31,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "\u4e2a\u4eba\u4fe1\u606f\u7c7b\u578b", namespace = "http://www.nju.edu.cn/schema")
+@XmlType(name = "个人信息类型", namespace = "http://www.nju.edu.cn/schema")
 public class 个人信息类型
     extends 个人基本信息类型
 {
@@ -47,7 +47,7 @@ public class 个人信息类型
         this.set地址(new 地址类型(entity.getAddress()));
         this.set姓名(entity.getName());
         this.set婚姻状况(婚姻类型.fromValue(entity.getMarriageStat()));
-        this.set性别(性别类型.fromValue(entity.getMarriageStat()));
+        this.set性别(性别类型.fromValue(entity.getSex()));
         this.set教育背景(教育类型.fromValue(entity.getEducation()));
         this.set部门(new 部门类型(departmentEntity.getManager(), departmentEntity.getDescription(), 部门属性类型.fromValue(departmentEntity.getAttr()), departmentEntity.getDptId(), departmentEntity.getName()));
         this.set手机号码(entity.getPhoneNum());
